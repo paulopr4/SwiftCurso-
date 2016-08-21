5f92691362e066577af6eb9d248f40bd18f9db94
Paulo Ricardo <paulo.pr4@hotmail.com>
2016-08-21 17:22:20 -0300

    Comando Swift

---
+++ 2.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// 2. Variáveis e Constantes
+
+var str2  = "Hello, playground now "
+
+var string2 : String = " The winter is coming!"
+
+var numeroIneiro = 42
+
+var numeroInteiro2 : Int = 10 // Int é o tipo usado para representar 
+
+var soma = numeroIneiro + numeroInteiro2
+
+var numeroDecima = 1.2
+var numeroDecimal2 : Double = 22.4
+
+
+var soma2 = Double(numeroIneiro) + numeroDecima
+
+var ligado = true
+var fechado = false
+
+let stringConstante = "Winterfell"
