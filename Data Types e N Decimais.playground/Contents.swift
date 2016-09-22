89111a5fc3e34bc7640d43b75e87b546f78b4980
Paulo Ricardo <paulo.pr4@hotmail.com>
2016-09-22 00:27:13 -0300

    Operadores e módulo

--- Data Types e N Decimais.playground/Contents.swift
+++ Data Types e N Decimais.playground/Contents.swift
@@ -48,3 +48,136 @@ var segundoBoolean : Bool = false
 
 
 
+// Diferença entre Floats vs Doubles
+
+// Double tem a capacidade de armazenar mais numeros do que Float
+
+var floatValeu : Float = 3.1415926535
+var doubleValue : Double = 3.14159266535
+
+print(floatValeu)
+
+print(doubleValue)
+
+// Exerício ´Variáveis Implicitas vs Explícitas 
+
+// Exercícios de Vari´veis Implícitas e Explícitas
+
+// Crie variáveis / constantes implícitas para os seguintes tipos de dados : String, Int, Double e Bool
+
+var carros = "Audio Q3"
+
+var velocidade = 200
+
+var totalgasolina = 4.0
+
+var kilometragem = 40.4040404040404
+
+var carroBonito = true
+
+
+// exemplo 2
+
+var nome = "Paulo"
+
+var idade = 26
+
+var peso = 68.5
+
+var homem = true
+
+
+
+
+// Variáveis Explicitas
+
+var carros2 : String = "Mercedes"
+
+var velocidade2 : Int = 300
+
+var totalgasolina2 : Float = 5.0
+
+var kilometragem2 : Double = 150.303030303
+
+// exemplo 2
+
+let nomeFeminino : String = "Rafaela"
+let idadeDeRafaela : Int =  23
+let pesoDeRafaela : Double =  63.7
+let alturaDeRafaela : Float = 293.4566666
+let homem2 : Bool = false
+
+// Basics Operators 21.09. 2016 
+
+// + , -, *, / operadores
+
+var a = 10
+var b = 20
+
+a + b
+a - b
+a * b
+a / b
+
+// 2. Operador Módulo
+// % este Operador retorna o resto de um cálculo especifico 
+// ele é usado em condicionais e para descobrir se um valor é par ou impar
+// Ele trabalha subtraindo um determinado valor de um numero segue exemplo abaixo
+
+b % a
+
+100 % 30
+
+11 % 2
+
+20 % 10
+
+// 3. Operando e Assinalando : É 
+
+// +=, -=, *=, /=
+
+/* a += 1
+
+b -= 2
+
+a /= 2
+ */
+
+
+b /= a
+
+b *= 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
