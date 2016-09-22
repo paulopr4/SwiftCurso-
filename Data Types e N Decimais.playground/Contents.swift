b814b02fd820314bc865e0fdf3e793d968b3a5ba
Paulo Ricardo <paulo.pr4@hotmail.com>
2016-09-21 23:31:39 -0300

    Aprendendo Swift atualizado

--- Data Types e N Decimais.playground/Contents.swift
+++ Data Types e N Decimais.playground/Contents.swift
@@ -3,3 +3,48 @@
 import UIKit
 
 var str = "Hello, playground"
+
+// Declaração Implícitas quando não é colocado a sua tipagem
+
+var texto = " Olá, playground"
+
+var numero  = 10
+
+var numero2 = 5.3
+
+var numero3 = 10.6
+
+
+// Declaração Explícitas quando colocamos o tipo que a variável possui
+
+let numero4 : Int = 10
+
+// Mais exemplos de Declarações Implícitas
+
+//String
+var meuSobreNome = "Ricardo"
+//Int
+var numero5 = 1000
+// Double
+var numero6 = 520.6
+// Bool
+var verdadeiro = true
+
+// Mais exemplos de Declaração Explícita
+
+//String
+var nomeDoMueCachorro : String = "Brutus"
+//Int
+var numero7 : Int = 50
+// Float
+var minhaFloat : Float = 5.6
+// Dlouble
+var minhaDouble : Double = 505.6
+//Bool
+var segundoBoolean : Bool = false
+
+// explicação para a diferença entre as formas Implícitas e Explicitas 12:00
+
+
+
+
